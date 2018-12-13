e7aa41cde48cde424fba0148daa04ba3612585f8
moise <moise@192.168.1.93>
2018-12-13 23:13:40 +0100

    Last commit before Karamoko checking. I need to implement  the testCases but no test package importe avalaible.

---
+++ CombatEpique/src/main/java/com/nelson/test/JeuTest.java
@@ -0,0 +1,24 @@
+package com.nelson.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JeuTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testJouer() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ CombatEpique/src/main/java/com/nelson/test/RodeurTest.java
@@ -0,0 +1,24 @@
+package com.nelson.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class RodeurTest {
+
+	@BeforeClass
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
